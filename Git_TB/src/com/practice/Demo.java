b44021cfcd3703c2a284f956b945d3f185cc852e
PrajyotSelenium <90840446+PrajyotSelenium@users.noreply.github.com>
2022-06-20 03:13:16 +1030

    Update Demo.java

--- Git_TB/src/com/practice/Demo.java
+++ Git_TB/src/com/practice/Demo.java
@@ -1,10 +1,12 @@
 package com.practice;
 
 public class Demo {
-
+        int x=1;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Demo project");
+		Demo d=new Demo();
+		System.out.println("Instance variable = "+d.x);
 	}
 
 }
